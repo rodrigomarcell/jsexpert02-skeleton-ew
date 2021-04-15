a88c93f11e3b94270114291bf44001d66d3eeef8
Rodrigo Marcel <rodrigomarcell@outlook.com>
2021-04-15 17:54:44 -0400

    inicio peer js

--- aula02/public/pages/room/src/app.js
+++ aula02/public/pages/room/src/app.js
@@ -16,11 +16,21 @@ const onload = () => {
   // recorderBtn.addEventListener('click', recordClick(recorderBtn));
 
   const socketUrl = 'http://localhost:3000';
-  const socketBuilder = new SocketBuilder({
-    socketUrl
+  
+  const socketBuilder = new SocketBuilder({socketUrl});
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/'
+    }
   });
+
+  const peerBuilder = new PeerBuilder({peerConfig});
   const view = new View();
-  const media = new Media(); 
+  const media = new Media();
 
 
   const deps = {
---
+++ aula02/public/pages/room/src/util/peer.js
@@ -0,0 +1,5 @@
+class PeerBuilder {
+    constructor({}) {
+
+    }
+}
